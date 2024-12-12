24b281692749f0cd445fba55b0ba35a30650b0ff
root <root@jesusm.com>
2024-12-12 12:10:31 -0400

    Route_inicio

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PacienteController;
+use App\Http\Controllers\ExamenController;
+use App\Http\Controllers\DatoExamenController;
+use App\Http\Controllers\SolicitudController;
+use App\Http\Controllers\DetalleSolicitudController;
+use App\Http\Controllers\ResultadoExamenController;
+use App\Http\Controllers\UsuarioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +20,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// FALTA
+/*
+    +Agrupacion de rutas por acceso
+    +Cambiar nombre de rutas
+    +Cambios en desarrolllo
+*/
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -20,3 +34,20 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Rutas para el módulo de pacientes
+Route::resource('pacientes', PacienteController::class);
+
+// Rutas para el módulo de exámenes
+Route::resource('examenes', ExamenController::class);
+Route::resource('datos-examenes', DatoExamenController::class);
+
+// Rutas para el módulo de solicitudes
+Route::resource('solicitudes', SolicitudController::class);
+Route::resource('detalle-solicitudes', DetalleSolicitudController::class);
+
+// Rutas para el módulo de resultados de exámenes
+Route::resource('resultados-examenes', ResultadoExamenController::class);
+
+// Rutas para el módulo de usuarios
+Route::resource('usuarios', UsuarioController::class);
